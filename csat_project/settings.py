66165d2b9f85adb23e1d315b778605c52af19da1
patelbhumika472@gmail.com <patelbhumika472@gmail.com>
2021-07-15 21:12:42 +0530

    commit by bhumika

--- csat_project/settings.py
+++ csat_project/settings.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
+
+Bhumika has put this comment
 """
 
 from pathlib import Path
+import dns.resolver
+dns.resolver.default_resolver=dns.resolver.Resolver(configure=False)
+dns.resolver.default_resolver.nameservers=['8.8.8.8']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
