4589d96eb83e87aeac11e46349563e88750fc3f4
Bhumika5297 <patelbhumika472@gmail.com>
2021-08-04 22:32:21 -0400

    new models ( retails and donut ) added

--- frontend/admin.py
+++ frontend/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Beverages, Item
+from .models import Beverages, Donut, Item, Retail
 
 # Register your models here.
 
 admin.site.register(Item)
 admin.site.register(Beverages)
+admin.site.register(Donut)
+admin.site.register(Retail)
--- frontend/models.py
+++ frontend/models.py
@@ -11,6 +11,26 @@ DRINK_TYPE = (
    ('SPECIAL','SPECIAL'),
    ('DONUT','DONUT')
 )
+
+DONUT_TYPE=(
+    ('SPECIALITY', 'SPECIALITY'),
+    ('RING FILLED', 'RING FILLED'),
+    ('CLASSIC', 'CLASSIC')
+)
+
+QUANTITY =(
+    ('1KG','1KG'),
+    ('5KG','2KG'),
+    ('10KG','10KG')
+)
+
+SANDWITHCHES = (
+    ('WHITE','WHITE'),
+    ('WHOLE WHEAT','WHOLE WHEAT'),
+    ('MULTIGRAIN','MULTIGRAIN'),
+)
+
+
 class Item(models.Model):
     item_type = models.CharField(max_length=200, choices=DRINK_TYPE )
 
@@ -30,3 +50,28 @@ class Beverages(models.Model):
     def __str__(self):
         return self.name
 
+
+class Donut(models.Model):
+    name = models.CharField(max_length=255)
+    type = models.CharField(choices=DONUT_TYPE,max_length=255)
+    price = models.FloatField()
+    desc = models.TextField()
+    calories = models.FloatField()
+
+
+    def __str__(self):
+        return self.name
+
+
+class Retail(models.Model):
+    name = models.CharField(max_length=255)
+    quantity = models.CharField(choices=QUANTITY,max_length=255)
+    price = models.FloatField()
+    desc = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+
+
+    
